c914cddf90145c31ae9d5ee5c3f22aa992fe98df
Lauren <Laurenelee88@gmail.com>
2018-01-04 13:59:08 -0800

    styling

--- src/components/AgencyDetail.js
+++ src/components/AgencyDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AgencyDetail = (props) => {
   return (
     <Card>
-    <Text>{props.agency.name}</Text>
+      <CardSection>
+        <Text>{props.agency.name}</Text>
+      </CardSection>
     </Card>
   );
 };
