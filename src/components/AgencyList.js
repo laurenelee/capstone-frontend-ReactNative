41e178c7ca4c34bb4c32c96c2976ffa043d90760
Lauren <Laurenelee88@gmail.com>
2018-01-04 12:43:40 -0800

    be careful with comments as babel does not like them. rendering list of 20 names

--- src/components/AgencyList.js
+++ src/components/AgencyList.js
@@ -4,17 +4,30 @@ import axios from 'axios';
 
 // const AgencyList = () => {  (commented out 1/4/18 when learning class components as opposed to functional component)
 class AgencyList extends Component {
+  state = { agencies: [] }; // step 1: initial state default created (empty list of agencies)
+
   // must have render method that returns some  amount of JSX (to make class based component)
   // lifecyle methods that automatically get called
   componentWillMount() {
     // console.log('component will mount in agency list');
     axios.get('http://localhost:3000/agency_details')
-    .then(response => console.log(response));
+    .then(response => this.setState({ agencies: response.data })); // step 2: updating component state and rerendering with new data (never do this.state = something for modification)
   }
+
+  // helper method to create list
+  renderAgencies() {
+    return this.state.agencies.map(agency =>
+      <Text key={agency.name}>{agency.name}</Text>
+    );
+  }
+
   render() {
+    console.log(this.state); // rendering twice (once empty and then full)
+    // now time to build a list of components! transform for each agency a component
+    //  must use .map method
     return (
       <View>
-      <Text>Agency List!! </Text>
+      {this.renderAgencies()}
       </View>
     );
   }
